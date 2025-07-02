51bf729266fdbfd1f126a75fa09714eb7ad9e363
Essie Lee <subinl@andrew.cmu.edu>
2025-07-02 16:24:58 -0500

    6 charts and weather widget displayed on dashboard

--- Data/SolarData.cs
+++ Data/SolarData.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.ObjectModel;
+
 namespace seirin1.Data
 {
     public class SolarData
@@ -22,4 +24,20 @@ namespace seirin1.Data
         /// </summary>	
         public double Height { get; set; }
     }
+    public class PowerReading
+    {
+        public DateTime Time { get; set; }
+        public double InverterPower { get; set; }
+        public double FeedinPower { get; set; }
+        public double LoadPower { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public string Icon { get; set; }  // "sunny.png", "cloudy.png", etc.
+        public double Temperature { get; set; }
+        public double MinTemp { get; set; }
+    }
+
 }
--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,11 +1,22 @@
+﻿using seirin1.Data;
 using System.Collections.Generic;
-using seirin1.Data;
+using System.Collections.ObjectModel;
 
 namespace seirin1.ViewModels
 {
     public class DashboardViewModel
     {
         public List<Person> Data { get; set; }
+        public ObservableCollection<PowerReading> PowerData { get; set; }
+
+        public string Location { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public double CurrentTemp { get; set; }
+        public string MinMaxTemp { get; set; }
+        public ObservableCollection<WeatherForecast> Forecast { get; set; }
+
+
+
 
         public DashboardViewModel()
         {
@@ -17,6 +28,29 @@ namespace seirin1.ViewModels
                 new Person { Name = "Joel", Height = 182 },
                 new Person { Name = "Bob", Height = 134 }
             };
+
+            PowerData = new ObservableCollection<PowerReading>
+            {
+                new PowerReading { Time = DateTime.Today.AddHours(5), InverterPower = 3000, FeedinPower = -2000, LoadPower = 5000 },
+                new PowerReading { Time = DateTime.Today.AddHours(6), InverterPower = 4000, FeedinPower = -1000, LoadPower = 6000 },
+                new PowerReading { Time = DateTime.Today.AddHours(7), InverterPower = 8000, FeedinPower = 1000, LoadPower = 7000 },
+                // ... more data points
+            };
+
+            Location = "Austin";
+            CurrentDate = DateTime.Today;
+            CurrentTemp = 25;
+            MinMaxTemp = "15 ℃ ~ 25 ℃";
+
+            Forecast = new ObservableCollection<WeatherForecast>
+            {
+                new WeatherForecast { Date = DateTime.Today.AddDays(1), Icon = "dotnet_bot.png", Temperature = 30, MinTemp = 16 },
+                new WeatherForecast { Date = DateTime.Today.AddDays(2), Icon = "mousty.jpeg", Temperature = 30, MinTemp = 16 },
+                new WeatherForecast { Date = DateTime.Today.AddDays(3), Icon = "dotnet_bot.png", Temperature = 29, MinTemp = 14 },
+            };
         }
+
+
+    
     }
 }
